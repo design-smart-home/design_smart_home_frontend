7ddee5b4ba813cfcf289dbbc2d8d8cf79a30184c
evoldeathh <tarstan01@mail.ru>
2025-02-06 19:49:53 +0500

    ScenarioPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ScenarioPage from './pages/ScenarioPage';
 import NotFound from './pages/NotFound';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/scenarios" element={<ScenarioPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
 );
-
 
 export default App;
