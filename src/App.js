4bc11ebba6bd5d59e735cf2ae63ea5152bace874
evoldeathh <tarstan01@mail.ru>
2025-02-10 00:29:38 +0500

    Request homepage scenariopage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ScenarioPage from './pages/ScenarioPage';
+import DevicesPage from './pages/DevicesPage'; // Импортируем новую страницу
 import NotFound from './pages/NotFound';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/scenarios" element={<ScenarioPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<HomePage />} /> 
+      <Route path="/scenarios" element={<ScenarioPage />} /> 
+      <Route path="/devices" element={<DevicesPage />} /> 
+      <Route path="*" element={<NotFound />} /> 
     </Routes>
   </Router>
 );
